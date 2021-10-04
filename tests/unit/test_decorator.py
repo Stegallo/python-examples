06cc78a89eff5600a3d96499ee1e7e4f68b2aec6
Stefano Gallotti <sgallotti@expediagroup.com>
2021-10-03 20:42:27 -0700

    add decorator

---
+++ tests/unit/test_decorator.py
@@ -0,0 +1,17 @@
+from unittest.mock import call, patch
+
+from python_examples.decorator import main
+
+
+def test_main():
+    with patch("python_examples.decorator.log") as mock_log:
+        main()
+
+    assert mock_log.call_args_list == [
+        call("Something is happening before the function is called."),
+        call("hi John"),
+        call("Something is happening after the function is called."),
+        call("Something is happening before the function is called."),
+        call("hello Doe"),
+        call("Something is happening after the function is called."),
+    ]
